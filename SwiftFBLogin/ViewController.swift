2d6a7fc7b6bcd435cf223580eab2edcd53d028db
strawberrypie <kjdr@kth.se>
2014-12-30 13:49:20 +0100

    Initial Commit

---
+++ SwiftFBLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftFBLogin
+//
+//  Created by Kj Drougge on 2014-12-30.
+//  Copyright (c) 2014 kj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
