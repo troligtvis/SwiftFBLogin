1066a288f089176bb0f0a252d7f8545db435c8b7
strawberrypie <kjdr@kth.se>
2014-12-30 14:03:12 +0100

    Facebook login and profilepicture view

---
+++ SwiftFBLogin/Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  SwiftFBLogin
+//
+//  Created by Kj Drougge on 2014-12-30.
+//  Copyright (c) 2014 kj. All rights reserved.
+//
+
+#ifndef SwiftFBLogin_Bridge_Header_h
+#define SwiftFBLogin_Bridge_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
--- SwiftFBLogin/ViewController.swift
+++ SwiftFBLogin/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBLoginViewDelegate {
 
+    @IBOutlet var fbLoginView: FBLoginView!
+    @IBOutlet var profilePictureView: FBProfilePictureView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+        self.profilePictureView.profileID = "ca.abernathy"
+    }
+    
+    func loginViewShowingLoggedInUser(loginView: FBLoginView!) {
+        println("Login")
+    }
+    
+    func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+        println("Username: \(user.name)")
+        self.profilePictureView.profileID = user.objectID
+        
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
+        println("Logout")
+        self.profilePictureView.profileID = nil
+    }
+    
+    func loginView(loginView: FBLoginView!, handleError error: NSError!) {
+        println("Error: \(error.localizedDescription)")
     }
 
     override func didReceiveMemoryWarning() {
